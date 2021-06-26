8e5d18925ef4ac5e2605a7059518c46003e39f4e
Molnár István <67184747+iiistvan@users.noreply.github.com>
2021-06-26 23:02:52 +0200

    python2 26/3 feladat

---
+++ python2-homework/fajl3.py
@@ -0,0 +1,6 @@
+# adat.txt beolvasása és tárolása listába, majd kiírása egy sorba
+
+with open("adat.txt", "r") as file1:
+    szoveg = file1.read().splitlines()
+with open("adat2.txt", "a") as file2:
+    file2.write(" ".join(szoveg))
